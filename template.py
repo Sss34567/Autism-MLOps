8783fa0b6b98e277e14f640291aa18c5c46caa3c
samah <ggsamah0@gmail.com>
2026-09-15 13:35:41 +0300

    folder structure added

--- template.py
+++ template.py
@@ -24,7 +24,7 @@ list_of_files=[
     f"{project_name}/pipline/prediction_pipline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
-
+"requirements.txt"
 ]
 
 for filepath in list_of_files:
