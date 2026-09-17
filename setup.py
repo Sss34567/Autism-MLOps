e044d27abb59be6363b81796855d4aae2004a85b
samah <ggsamah0@gmail.com>
2026-09-17 10:29:06 +0300

    folder a

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Autism-dig",
+    version="0.0.0",
+    author="samah",
+    author_email="ggsamah0@gmail.com",
+    packages=find_packages()
+)
